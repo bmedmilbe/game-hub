eb7b61934976a812d245b5c5b882c8ead715e387
Edmilbe <edmilbe14@hotmail.com>
2024-12-09 00:23:41 +0000

    Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
+import Demo from "./components/Demo";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+          Nav
+        </GridItem>
+        <GridItem hideBelow="lg" area="aside" bg="gold">
+          Aside
+        </GridItem>
+        <GridItem area="main" bg="dodgerblue">
+          Main
+        </GridItem>
+      </Grid>
+    </>
+  );
 }
 
 export default App;
