f1283506f04b40c3f4e0662acfeec6897aae0e8d
Edmilbe <edmilbe14@hotmail.com>
2024-12-13 03:25:57 +0000

    Set Logo

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
           lg: `"nav nav" "aside main"`,
         }}
       >
-        <GridItem area="nav" bg="coral">
+        <GridItem area="nav">
           <NavBar />
         </GridItem>
         <GridItem hideBelow="lg" area="aside" bg="gold">
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,10 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
 
 const NavBar = () => {
   return (
     <HStack>
-      <Image src={"https://cdn-icons-png.flaticon.com/128/1828/1828884.png"} />
+      <Image src={logo} boxSize={"60px"} />
       <Text>Nav Bar</Text>
     </HStack>
   );
